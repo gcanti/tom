ed143c8590b09c64d28647231bf7ce56bcc4a7cb
gcanti <giulio.canti@gmail.com>
2015-01-11 17:06:57 +0100

    export default configuration

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
+'use strict';
+
 var t = require('tcomb');
+var Router = require('./lib/Router');
+var matcher = require('./lib/matcher');
+var EventEmitter = require('eventemitter3');
 
 t.om = {
-  Router: require('./lib/Router')
+  Router: function () {
+    return new Router({
+      matcher: matcher,
+      emitter: new EventEmitter()
+    });
+  }
 };
 
 module.exports = t;
