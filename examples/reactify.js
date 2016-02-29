47c71d488963cadab77183a2c5a9bb290fa21e4d
gcanti <giulio.canti@gmail.com>
2016-02-29 09:49:27 +0100

    fix init

--- examples/reactify.js
+++ examples/reactify.js
@@ -1,44 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { start } from 'tom'
-
-const counter = {
-
-  init() {
-    return { model: 0 }
-  },
-
-  update(model, event) {
-    switch (event) {
-    case 'INCREMENT' :
-      return { model: model + 1 }
-    case 'DECREMENT' :
-      return { model: model - 1 }
-    default :
-      return { model }
-    }
-  },
-
-  view(model, dispatch) {
-    const increment = () => dispatch('INCREMENT')
-    const decrement = () => dispatch('DECREMENT')
-    return (
-      <div>
-        <p>Counter: {model}</p>
-        <button onClick={increment}>+1</button>
-        <button onClick={decrement}>-1</button>
-      </div>
-    )
-  }
-
-}
+import counter from './counter'
 
 function reactify(config) {
-  return class RxComponent extends React.Component {
+  return class TomComponent extends React.Component {
 
     app = start(config)
     state = {
-      view: config.view(config.init.model, this.app.dispatch)
+      view: config.view(config.init().model, this.app.dispatch)
     }
 
     componentDidMount() {
